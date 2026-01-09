1b1995c0c428ee9a9d32e0ed4ba8a9fd4574e677
vanshu91 <vanshikayadav045@gmail.com>
2026-01-09 14:48:52 +0530

    Add files via upload

---
+++ unit2FLOWCONTROL (1) (1) (1).py
@@ -0,0 +1,88 @@
+# temprature="25C"
+
+# if  temprature=="24C":
+#     print("mid")
+# elif    temprature<="25C":
+#     print("normal")
+# else:
+#      print("cold")
+     
+# vehicle=input("enter vehicle")
+# if  vehicle in("tvs","hero","bullet"): 
+#     print("two wheeler")
+# elif vehicle in("BMW","THAR","MAHENDRA"):
+#     print("four wheeler")
+# else:
+#     print("unknown vehicle")
+
+# bal=6000
+# withdraw=int(input("enter amount to withdraw"))
+# if  withdraw<=bal:
+#     print("transaction successfully")
+# else:
+#     print("insufficient Balance") 
+
+                #  movie ticket disc.
+
+# age=input("enter age")
+# if  age<="12":
+#     print("your ticket price is 100")
+# elif    age=="12":
+#     print("your ticket price is 150")
+# elif age<="18":
+#     print("your ticket price is 200")
+# elif    age=="18":
+#     print("your ticket price is 300")
+# elif    age=="20":
+#     print("your ticket price is 450")
+# elif    age>="20":
+#     print("your ticket price is 520")
+# elif    age>="25":
+#     print("your ticket price is 700")
+# else:
+#     print("invalid")
+
+# time=int(input("enter current time"))
+
+# if  time>="09:00AM":
+#     print("institute open")
+# else:
+#     time>="05:00PM"
+#     print("institute closed")
+
+                    #    temperature checker
+# temp=int(input("current temp "))
+# if  temp>30:
+#     print("it's a hot day!")
+# elif    temp<=20:
+#     print("nice weather!")
+# elif    temp<=30:
+#     print("normal weather!")
+# else:
+#     print("it's cold today!")
+
+# num=int(input("enter number"))
+# if  num % 2==0:
+#     print("even num")
+# else:
+#     print("odd num")
+
+# marks=int(input("enter your marks"))
+# if  marks>90:
+#     print("Grade A")
+# elif  marks>75:
+#     print("Grade B")
+# elif    marks>50:
+#     print("Grade C")
+# else:
+#     print("Fail")
+    
+# signal_colour=input("enter signal colour : ")
+# if  signal_colour == "red":
+#     print( "stop")
+# elif    signal_colour == "yellow":
+#     print("wait")
+# elif    signal_colour == "green":
+#     print( "GO")
+# else:
+#     print( "invalid")
