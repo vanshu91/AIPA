3d70597bef4b6a3b8411d5c6ec5b5557ac170b7f
vanshu91 <vanshikayadav045@gmail.com>
2026-01-09 14:48:06 +0530

    Add files via upload

---
+++ LoopConcept (1).py
@@ -0,0 +1,52 @@
+# user=1
+# while user<=3:
+#     print("user:", user)
+#     bal=1000
+#     for i in range(1,4):
+#         amount=int(input("enter your amount: "))
+#         if amount <= bal:
+#             bal=bal-amount
+#             print("remaining balance",bal)
+#         else:
+#             print("insufficient balance")
+#     print("user:", user, "transaction compeleted")
+#     user +=1
+
+# user=1 
+# orignalbal={1:1000,2:6000, 3:8000}
+# while user<=3:
+#     print("user:", user)
+#     bal=orignalbal[user]
+#     for i in range(1,4):
+#         amount=int(input("enter your amount: "))
+#         if amount <= bal:
+#             bal=bal+amount
+#             print("remaining balance",bal)
+#         # else:
+#         #     print("insufficient balance")
+#     print("user:", user, "transaction compeleted")
+#     user +=1
+
+# customer=1
+# while customer<=2:
+#     print("costomer", customer)
+#     total=0
+#     for i in range(1,4):
+#         amount=int(input("enter your amount"))
+#         total=total+amount
+#         print("total bill: ", total)
+#     print("next customer")
+#     customer+=1
+
+# student=1
+# while student<=3:
+#     print("\n student marks:", student)
+#     total=0
+#     for i in range(1,6):
+#         marks=int(input("enter marks"))
+#         total=total+marks 
+#     avg=total/5
+#     print("total marks:", total)
+#     print("average marks: ", avg)
+#     print("another student")
+#     student+=1
