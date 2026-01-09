12de9905afc3694582b5c9459db5e3594eda3a0a
vanshu91 <vanshikayadav045@gmail.com>
2026-01-09 14:43:40 +0530

    Add files via upload

---
+++ labpractice3 (1).py
@@ -0,0 +1,93 @@
+# int1 = 10
+# int2 = -5
+# int3 = 0
+# float1 = float(int1)
+# float2 = float(int2)
+# float3 = float(int3)
+# # print("orignal integer value:")
+# # print("int1 =" , int1)
+# # print("int2 =" , int2)
+# # print("int3 =" , int3) 
+# print("\nconverted float values:")
+# print("float1 =", float1)
+# print("float2 =", float2)
+# print("float3 =", float3) 
+# integer1 = 10 
+# integer2 = -5
+# integer3 = 0
+# # print("integer value:") 
+# # print("integer1 =", integer1)
+# # print("integer2 =", integer2)
+# # print("integer3 =", integer3)
+# print("sum of integer1 and integer2:", integer1 + integer2)
+# print("product of integer1 and 3:", integer1 * 3)
+# print("Difference between integer1 and integer2: ", integer1 - integer2) 
+# float1 = 3.14
+# float2 = 2.718
+# float3 = 0.0
+# sum_floats = float1 + float2 
+# product_floats = float1 * 2 
+# difference_floats = float1 - float2 
+# print("\n floating-point values:")
+# print("float1 = ", float1)
+# print("float2 = ", float2)
+# print("float3 = ", float3)
+# print("sum of float1 and float2:", sum_floats)
+# print("product of float1 and float2:", product_floats)
+# print("difference between float1 and float2", difference_floats) 
+# single_qouted = 'hello, world!'
+# length = len(single_qouted) 
+# upper_case = single_qouted. upper() 
+# lower_case = single_qouted. lower() 
+# contains_substring = 'world' in single_qouted 
+# print("single-qouted string:", single_qouted)
+# print("length of the string:", length)
+# print("uppercase version:", upper_case)
+# print("lowercase version:", lower_case) 
+# print("contains 'world':", contains_substring)
+
+                # ARITHMATIC 
+# x=10
+# y=5
+# print(x+y)
+# print(x-y)
+# print(x*y)
+# print(x/y)
+# print(x**y)
+# print(x//y)
+# print(x%y)
+                #    COMPARATIVE
+# print(x==y)
+# print(x>y)
+# print(x!=y)
+# print(x>=y)
+# print(x<y)
+# print(x<=y)
+#                    # LOGICAL
+# a=5
+# b=6
+# c=7 
+# print(a>b and c>b) 
+# print(a>b or c>a)
+# print(not c>a)
+# print(a<b and c>b)
+# x=10
+# y=5
+# x+=y
+# print(x)
+# x-=y
+# print(x)
+# x*=y
+# print(x)
+# x%=y 
+# print(x)
+# x**=y
+# print(x)
+# x//=y
+# print(x)
+# #                                   FOR EXAMPLE 
+# x=20
+# # y=10 
+# x+=y
+# print(x) 
+        
